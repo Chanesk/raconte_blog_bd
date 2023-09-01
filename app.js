237314d44765b10679ef1b81374beafe670de8b4
Chanesk <chanesk01@gmail.com>
2023-09-01 20:11:27 +0300

    fix: utilisation des variable d'environement

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const raconteRoutes = require('./routes/raconte');
 const userRoutes = require('./routes/user')
-
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://admin:xM8gSCsVESaCvuVX@cluster0.ta1dmua.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNEXION,
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+  
   app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
