ae69b321ea262cb47ef27f105deaa9134e6d3546
Chanesk <chanesk01@gmail.com>
2023-09-01 22:59:36 +0300

    fix: implement de la connexion mongoose

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
+require('dotenv').config();
 const http = require('http');
-const app = require('./app');
-const { Console } = require('console');
+const express = require('express');
+const mongoose = require('mongoose');
+const raconteRoutes = require('./routes/raconte');
+const userRoutes = require('./routes/user');
+
+
+const app = express();
+app.use(express.json());
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_CONNEXION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+ app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.use('/api/raconte', raconteRoutes);
+app.use('/api/auth', userRoutes);
 
 const normalizePort = val =>{
     const port = parseInt(val, 10);
@@ -42,4 +73,7 @@ server.on('listening', ()=> {
     const bind = typeof address === 'string' ? 'pipe' + address : 'port ' +port;
     console.log('Listening on '+ bind);
 })
-server.listen(port);
+
+connectDB().then(() => {
+    server.listen(port);
+})
